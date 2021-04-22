f1aa27676ff797bcf2c4a70003fbe9eb09c77d3d
skiinder <skiinder@live.cn>
2021-04-22 15:38:42 +0800

    create.sql v1.1

--- myshell/create.sql
+++ myshell/create.sql
@@ -19,11 +19,11 @@ create table test.ind
     week_on_week_ratio     double comment '同比(周)'      default null,
     week_on_week_ratio_max double comment '同比指标上限'     default null,
     week_on_week_ratio_min double comment '同比指标下限'     default null,
-    std_div                double comment '标准差'        default null,
-    std_div_max            double comment '标准差指标上限'    default null,
-    std_div_min            double comment '标准差指标下限'    default null,
+    std_dev                double comment '标准差'        default null,
+    std_dev_max            double comment '标准差指标上限'    default null,
+    std_dev_min            double comment '标准差指标下限'    default null,
     consistency_table      varchar(20) comment '一致性父表' default null,
-    consistency_div        int comment '数据量差额'         default null,
+    consistency_dev        int comment '数据量差额'         default null,
     primary key (dt, tbl, col)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
